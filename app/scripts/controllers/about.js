6b2622dd97e0a7965873523050b43253b127446e
James Pants <jamespants@gmail.com>
2015-04-01 13:13:56 +0200

    about

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -9,9 +9,4 @@
  */
 angular.module('giantSteps2App')
   .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
