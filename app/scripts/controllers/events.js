b818c3acb0fa29ba0e6db7a5dc92ff69156c63cd
James Pants <jamespants@gmail.com>
2015-04-07 10:45:59 +0200

    more

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -7,11 +7,19 @@
  * # EventsCtrl
  * Controller of the giantSteps2App
  */
-angular.module('giantSteps2App')
-  .controller('EventsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('giantSteps2App').controller('EventsCtrl', function ($scope) {
+
+	$scope.calendar = [
+		{
+			title: 'CHI2015 Workshop: Collaborating with Intelligent Machines: Interfaces for Creative Sound',
+			posted: Date.now(),
+			author: 'admin',
+			date: ''
+
+		}
+
+
+	];
+
+
+});
--- app/scripts/directives/make-intro.js
+++ app/scripts/directives/make-intro.js
@@ -10,11 +10,20 @@ angular.module('giantSteps2App').directive('makeIntro', function () {
 
 	var link = function($scope){
 
+		var audioLib = {
+  			squish: ["sine",0.0000,0.4000,0.0000,0.3120,0.0000,0.2040,20.0000,85.0000,1794.0000,0.5420,-0.9740,0.6270,0.0100,0.0003,0.0000,0.0000,0.0610,0.5000,0.0520,0.0000,0.0000,0.0000,0.7580,0.0000,0.3480,0.2230,0.0000],
+  			out: ["sine",0.0000,0.4000,0.0000,0.3120,0.0000,0.2040,20.0000,20.0000,562.0000,0.5420,0.1580,0.6270,0.0100,0.0003,0.0000,0.0000,0.0610,0.5000,0.0520,0.0000,0.0000,0.0000,0.7580,0.0000,0.3480,0.2230,0.0000]
+  		};
+
+  		var samples = jsfxlib.createWaves(audioLib);
+  		
+
 		$scope.intro = {
 			main: true,
 			creativity: false,
 			experimentation: false,
 			performance: false,
+			reverse: false,
 
 			show: function(section){
 				var self = this;
@@ -33,15 +42,24 @@ angular.module('giantSteps2App').directive('makeIntro', function () {
 					self.main = false;
 					self.performance = true;
 				}
+
+				self.reverse = true;
+
+				samples.squish.play();
 			},
 
 			back: function(){
 				var self = this;
 
+				samples.out.play();
+
 				self.main = true;
 				self.creativity = false;
 				self.experimentation = false;
 				self.performance = false;
+				self.reverse = false;
+
+				
 			}
 		};
 	};
