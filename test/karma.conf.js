3e7bec3720285bf4e5a86e0104cdff925679ae11
James Pants <jamespants@gmail.com>
2015-06-03 18:43:49 +0200

    remove more

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -24,7 +24,6 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
-      'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/contentful/dist/contentful.min.js',
